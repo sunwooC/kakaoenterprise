27213cab8b37c45b49c0f42572c99e4a264df5f6
User1 <User1@host.docker.internal>
2021-07-07 17:16:23 +0900

    actuator 조회 후 로깅 처리

--- kakaoenterprise/src/main/java/com/kakaoenterprise/scheduler/Scheduler.java
+++ kakaoenterprise/src/main/java/com/kakaoenterprise/scheduler/Scheduler.java
@@ -2,18 +2,29 @@ package com.kakaoenterprise.scheduler;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Component
 public class Scheduler {
 	//초 분 시 일 월 주 년
-   @Scheduled(cron = "0 0/10 * * * *")
+   @Scheduled(cron = "0 0/5 * * * *")
    public void cronJobSchㅠSpringActuator() {
 	   //Spring Actuator
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
       Date now = new Date();
       String strDate = sdf.format(now);
-      System.out.println("Java cron job expression:: " + strDate);
+      
+      final String uri = "http://localhost:8080/actuator/health";
+      RestTemplate restTemplate = new RestTemplate();
+      String result = restTemplate.getForObject( uri, String.class);
+      log.info("'CHECK':{}",result);
    }
 }
